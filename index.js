704e6877cb6e4e72264621b92ebf8efd6f367d59
bharatroy4D <bharatroy1005@gmail.com>
2026-05-14 23:46:11 +0600

    Even or Odd number check

---
+++ index.js
@@ -0,0 +1,9 @@
+// problem-1.....even or odd
+function checkNumber(number) {
+    if (number % 2 === 0) {
+        return "event number" 
+    }else{
+        return "odd number"
+    }
+}
+// console.log(checkNumber(7));
